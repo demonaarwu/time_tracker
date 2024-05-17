0e71f03f7e2c87c744b9c99cf179728e5d3a1603
Aaron Wu <demonaarwu@gmail.com>
2024-05-17 15:05:00 +0800

    Create template for tracker.db

---
+++ template.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  username TEXT NOT NULL,
+  hash TEXT NOT NULL
+);
+CREATE TABLE labels(
+  user_id INTEGER NOT NULL,
+  label_name TEXT NOT NULL
+);
+CREATE TABLE history(
+  user_id INTEGER NOT NULL,
+  label_name TEXT NOT NULL,
+  time INTEGER NOT NULL,
+  date TEXT NOT NULL
+);
